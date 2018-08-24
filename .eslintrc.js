ccc8ea378d457c4684988571228cb05d16124d18
Žiga Krašovec <zkrasovec@gmail.com>
2018-08-24 09:52:53 +0200

    :rotating_light: Removed unwanted lint warnings

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+/* eslint-disable */
+module.exports = {
+	env: {
+		browser: true,
+		es6: true
+	},
+	extends: 'eslint:recommended',
+	parserOptions: {
+		ecmaVersion: 2017,
+		sourceType: 'module'
+	},
+	rules: {
+		'no-console': 0,
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'always']
+	}
+};
